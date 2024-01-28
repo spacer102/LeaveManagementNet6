0bc58623894d6409906b23646c475b2ea56b2b5a
Spacer102 <kocak.viliam@gmail.com>
2024-01-28 05:28:40 +0100

    Added Database Tables and Migrations.

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DataCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
